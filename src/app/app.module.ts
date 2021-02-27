0636e57b1528e7eefbec0ea8046f17ccb49c1e28
Gracielagchz <77737186+Gracielagchz@users.noreply.github.com>
2021-02-27 03:43:09 -0800

    Defining first component and model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentShowComponent } from './current-show/current-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentShowComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/icurrent-show.ts
@@ -0,0 +1,9 @@
+export interface ICurrentShow {
+  show: string
+  network: string
+  genres: string
+  image: string
+  episodes: number
+  description: string
+   
+}
